8a6a486783387826d09622f06582f2a62f8980d8
Caden Joergens <58443587+StellarRaccoon@users.noreply.github.com>
2026-02-18 17:29:59 -0500

    chp 2 octave

--- mavsim_matlab/chap2/mav_viewer.m
+++ mavsim_matlab/chap2/mav_viewer.m
@@ -15,7 +15,7 @@ classdef mav_viewer < handle
         %------constructor-----------
         function self = mav_viewer
             self.body_handle = [];
-            [self.Vertices, self.Faces, self.facecolors] = self.define_spacecraft();
+            [self.Vertices, self.Faces, self.facecolors] = self.define_mav();
             self.plot_initialized = 0;           
         end
         %---------------------------
@@ -63,16 +63,16 @@ classdef mav_viewer < handle
         function pts=rotate(self, pts, phi, theta, psi)
             % define rotation matrix (right handed)
             R_roll = [...
-                        1, 0, 0;...
-                        0, cos(phi), sin(phi);...
+                        1, 0, 0;
+                        0, cos(phi), sin(phi);
                         0, -sin(phi), cos(phi)];
             R_pitch = [...
-                        cos(theta), 0, -sin(theta);...
-                        0, 1, 0;...
+                        cos(theta), 0, -sin(theta);
+                        0, 1, 0;
                         sin(theta), 0, cos(theta)];
             R_yaw = [...
-                        cos(psi), sin(psi), 0;...
-                        -sin(psi), cos(psi), 0;...
+                        cos(psi), sin(psi), 0;
+                        -sin(psi), cos(psi), 0;
                         0, 0, 1];
             R = R_roll*R_pitch*R_yaw;   % inertial to body
             R = R';  % body to inertial
@@ -85,7 +85,7 @@ classdef mav_viewer < handle
             pts = pts + repmat([pn;pe;pd],1,size(pts,2));
         end
         %---------------------------
-        function [V, F, colors] = define_spacecraft(self)
+        function [V, F, colors] = define_mav(self)
             fuse_h = 3;
             fuse_w = 2;
             fuse_l1 = 5;
@@ -98,33 +98,40 @@ classdef mav_viewer < handle
             tailwing_w = 3;
             % Define the vertices (physical location of vertices)
             V = [
-                fuse_l1, 0, 0; % point 1
-                fuse_l2, fuse_w/2, -fuse_h/2; % point 2
-                fuse_l2, -fuse_w/2, -fuse_h/2; % point 3
-                fuse_l2, -fuse_w/2, fuse_h/2; % point 4
-                fuse_l2, fuse_w/2, fuse_h/2; % point 5
-                -fuse_l3, 0, 0; % point 6
-                0 ,wing_w/2,0; % point 7
-                -wing_l, wing_w/2, 0; % point 8
-                -wing_l, -wing_w/2, 0; % point 
-                0, -wing_w/2, 0; % point 10
-                -fuse_l3+tailwing_l, tailwing_w/2, 0; % point 11
-                -fuse_l3, tailwing_w/2, 0; % point 12
-                -fuse_l3, -tailwing_w/2, 0; % point 13
-                -fuse_l3+tailwing_l, -tailwing_w/2, 0; % point 14
-                -fuse_l3+tailwing_l, 0, 0;% point 15
-                -fuse_l3, 0, -tail_h; % point 16
+                fuse_l1, 0, 0;                              % point 1
+                fuse_l2, fuse_w/2, -fuse_h/2;               % point 2
+                fuse_l2, -fuse_w/2, -fuse_h/2;              % point 3
+                fuse_l2, -fuse_w/2, fuse_h/2;               % point 4
+                fuse_l2, fuse_w/2, fuse_h/2;                % point 5
+                -fuse_l3, 0, 0;                             % point 6
+                0, wing_w/2, 0;                             % point 7
+                -wing_l, wing_w/2, 0;                       % point 8
+                -wing_l, -wing_w/2, 0;                      % point
+                0, -wing_w/2, 0;                            % point 10
+                -fuse_l3+tailwing_l, tailwing_w/2, 0;       % point 11
+                -fuse_l3, tailwing_w/2, 0;                  % point 12
+                -fuse_l3, -tailwing_w/2, 0;                 % point 13
+                -fuse_l3+tailwing_l, -tailwing_w/2, 0;      % point 14
+                -fuse_l3+tailwing_l, 0, 0;                  % point 15
+                -fuse_l3, 0, -tail_h;                       % point 16
             ]';
 
             % define faces as a list of vertices numbered above
             F = [...
-                    1, 2,  3;  % front
-                    1, 3, 4;  % back
-                    1, 4,  5;  % right 
-                    1, 5,  2;  % left
-                    2, 5, 3;  % top
-                    3, 6, 4; % bottom
-                    ];
+                    1, 2, 3;        % top nose
+                    1, 3, 4;        % port nose
+                    1, 4, 5;        % bottom nose 
+                    1, 5, 2;        % starboard nose
+                    3, 6, 4;        % port fusalaage
+                    2, 3, 6;        % top fusalage
+                    2, 5, 6;        % starboard fusalage
+                    4, 5, 6;        % bottom fusalage 
+                    7, 8, 9;        % wing 1
+                    7, 9, 10;       % wing 2
+                    11, 12, 13;     % Horiz stabalizer 1
+                    11, 13, 14;     % h stabalizer 2
+                    6, 15, 16;      % v stabalizer
+                ];
 
             % define colors for each face    
             myred = [1, 0, 0];
@@ -134,13 +141,20 @@ classdef mav_viewer < handle
             mycyan = [0, 1, 1];
 
             colors = [...
-                myyellow;... % front
-                myblue;...   % back
-                myblue;...   % right
-                myblue;...   % left
-                myblue;...   % top
-                mygreen;...  % bottom
-                ];
+                myred;      % top nose
+                mygreen;    % port nose
+                myblue;     % bottom nose 
+                myyellow;   % starboard nose
+                myred;      % port fusalaage
+                mygreen;    % top fusalage
+                myblue;     % starboard fusalage
+                myyellow;   % bottom fusalage 
+                mycyan;     % wing 1
+                myyellow;   % wing 2
+                mycyan;     % Horiz stabalizer 1
+                myyellow;   % h stabalizer 2
+                myyellow;   % v stabalizer
+            ];
         end
     end
 end
